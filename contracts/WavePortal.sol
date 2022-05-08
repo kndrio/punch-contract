8b21f49a26fe99bd338cd790564fbd295fe76daf
Kennedy Carvalho <kennedy.carvalho@mprj.mp.br>
2022-05-08 02:23:18 -0300

    web3dev-waveportal-s1-3

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("WavePortal smart contract.");
+    }
+}
