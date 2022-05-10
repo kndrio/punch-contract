685cc7cc4bfbe801c39b131e5c1ddb3c4ad78aaf
Kennedy Carvalho <kennedy.carvalho@mprj.mp.br>
2022-05-10 01:37:59 -0300

    web3dev-waveportal-s1-4

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("WavePortal smart contract.");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s sends waves.", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("Total waves: %s", totalWaves);
+        return totalWaves;
     }
 }
