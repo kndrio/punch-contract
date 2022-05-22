d9728c69f0a5425eb46219b5f13e82bf78b99fe6
Kennedy Carvalho <kennedy.carvalho@mprj.mp.br>
2022-05-22 04:07:13 -0300

    web3dev-punchportal-s3-1

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,17 +7,35 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] waves;
+
     constructor() {
         console.log("WavePortal smart contract.");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s sends waves.", msg.sender);
+        console.log("%s send this punch: %s", msg.sender, _message);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("Total waves: %s", totalWaves);
+        console.log("Total punchs: %s", totalWaves);
         return totalWaves;
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ const main = async () => {
         const [deployer] = await hre.ethers.getSigners();
         const accountBalance = await deployer.getBalance();
 
-        console.log("Deploying constracts with account: ", deployer.address);
+        console.log("Deploying contracts with account: ", deployer.address);
         console.log("Account balance: ", accountBalance.toString());
 
         const Token = await hre.ethers.getContractFactory("WavePortal");
