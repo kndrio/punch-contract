84c3e1cc9fd0df0159b3163884c0f855af9801a0
Kennedy Carvalho <kennedy.carvalho@mprj.mp.br>
2022-05-22 05:03:44 -0300

    web3dev-punchportal-s4-1

--- scripts/run.js
+++ scripts/run.js
@@ -1,23 +1,37 @@
 const main = async () => {
-    //const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
     await waveContract.deployed();
+    console.log("Contract deployed to address:", waveContract.address);
 
-    console.log("Contract deployed to:", waveContract.address); 
-    //console.log("Contract deployed by:", owner.address);
+    //balance of the contract
+    let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(contractBalance));
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
     console.log(waveCount.toNumber());
 
+    //Send transactions
     let waveTxn = await waveContract.wave("Feel that punch!");
     await waveTxn.wait();
 
+    //Send transactions
+    let waveTxn2 = await waveContract.wave("Feel that punch!");
+    await waveTxn2.wait();
+
+    //randomPerson send transaction
     const [_, randomPerson] = await hre.ethers.getSigners();
     waveTxn = await waveContract.connect(randomPerson).wave("Take that punch!");
     await waveTxn.wait();
 
+    //balance after sending transactions
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(contractBalance));
+
+    //allWaves
     let allWaves = await waveContract.getAllWaves();
     console.log(allWaves);
 };
